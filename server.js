495f116b866e4b6e6a38f140353500221578f260
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-06-20 16:09:24 +0100

    Adds server.js. Can get saved countries & add to saved countries

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const parser = require('body-parser');
+const server = express();
+
+server.use(parser.json());
+server.use(parser.urlencoded({extended: true}));
+server.use(express.static('client/build'))
+
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+
+MongoClient.connect("mongodb://localhost:27017", function(err, client) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  db = client.db('bucket_list');
+  console.log('Connected to database!');
+
+  server.post('/api/bucket-list', function(req, res, next) {
+  const bucketList = db.collection('countries');
+  const newCountry = req.body;
+  bucketList.save(newCountry, function(err, result) {
+    if (err) next(err);
+    res.status(201);
+    res.json(result.ops[0])
+    console.log('saved  to database!!');
+  });
+});
+
+  server.get('/api/bucket-list', function(req, res, next) {
+    const bucketList = db.collection('countries');
+    bucketList.find().toArray(function(err, allCountries) {
+      if (err) next(err);
+      res.json(allCountries);
+    })
+  })
+
+
+  server.listen(3000, function(){
+    console.log("Listening on port 3000");
+  });
+})
